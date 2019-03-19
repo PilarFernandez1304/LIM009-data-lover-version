42dec56ae506faca3472bccea46535894100b336
PilarFernandez1304 <ccentepilar@gmail.com>
2019-03-19 00:29:26 -0500

    agregando a filtrado

--- src/data.js
+++ src/data.js
@@ -8,3 +8,30 @@ const example = () => {
 };
 
 window.example = example;
+//window.viewPokemones=viewPokemones;
+const viewPokemones = () =>{
+for (let i = 0; i <POKEMON.pokemon.length ; i++) {
+  const con=document.getElementById('root').innerHTML+=
+  ` <div class="conteiner-pokemon">
+      <p> ${POKEMON.pokemon[i].num}</p>
+      <img src="${POKEMON.pokemon[i].img}">
+      <p> ${POKEMON.pokemon[i].name}
+  
+    </div>
+  `;
+  
+  }
+}
+
+//Mostrar
+
+const FiltrarTipo =() =>{
+  
+  const resultFiltrado = POKEMON.pokemon.filter(FiltrarTipo=>
+  FiltrarTipo.type=='Grass' || FiltrarTipo.type=='Poison' || FiltrarTipo.type == 'Normal'
+  || FiltrarTipo.type=='Flying')
+  return resultFiltrado;
+}
+
+console.log(FiltrarTipo());
+
--- src/main.js
+++ src/main.js
@@ -1,16 +1,7 @@
 
 
- 
-for (let i = 0; i <POKEMON.pokemon.length ; i++) {
-const con=document.getElementById('root').innerHTML+=
-` <div class="conteiner-pokemon">
-    <p> ${POKEMON.pokemon[i].num}</p>
-    <img src="${POKEMON.pokemon[i].img}">
+const
 
-  </div>
-`;
-
-}
 
 
   
